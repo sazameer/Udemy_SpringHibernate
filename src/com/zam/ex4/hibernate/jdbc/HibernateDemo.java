37a3a436a21ad108a1d6a64c8f8f2a7bf4dc3185
Zameer <replyzameer@gmail.com>
2019-12-31 18:37:30 +0530

    Hibernate: Fetching data from sql based upon query criteria

--- src/com/zam/ex4/hibernate/jdbc/HibernateDemo.java
+++ src/com/zam/ex4/hibernate/jdbc/HibernateDemo.java
@@ -1,5 +1,7 @@
 package com.zam.ex4.hibernate.jdbc;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -19,24 +21,60 @@ public class HibernateDemo {
 		
 		try {
 			
+			//Step 1 : Insert a Record
+			/*
+			//a. Create a student entity object
 			Student st1 = new Student("Zameer1", "Shaik", "test1@gmail.com");
 			
+			//b. Begin Transaction
 			session.beginTransaction();
 			
+			//c. Save the entity object
 			session.save(st1);
 			
+			//d. Commit the transaction
 			session.getTransaction().commit();
+			*/
 			
+			//Step 2 : Retrieve data from database
+			/*
+			//a. Get Current Session
 			session = sessionFactory.getCurrentSession();
 			
+			//b. Begin Transaction
 			session.beginTransaction();
 			
-			Student newStudent = session.get(Student.class, st1.getId());
+			//c. Using the id from the above saved object get the student from the database
+			Student newStudent = session.get(Student.class, 1);
 			
+			//d. Print student
 			System.out.println(newStudent);
 			
+			//e. Commit the transaction
 			session.getTransaction().commit();
+			*/
 			
+			//Step 3: HQL example
+			//Fetching all the records from the students table and displaying
+			
+			//a. Get Session
+			session = sessionFactory.getCurrentSession();
+			
+			//b. Begin Transaction
+			session.beginTransaction();
+			
+			//c. Fetch Results
+			//String query = "from Student s";  							//Plain Query
+			//String query = "from Student s where s.email like '%2%'";		//Single Condition
+			String query = "from Student s where s.email like '%2%' OR s.firstName like '%4%'"; //Multiple Conditions
+			List<Student> studentsList = session.createQuery(query).getResultList();
+			
+			//d. Iterate over the results
+			for(Student student : studentsList) {
+				System.out.println(student);
+			}
+			
+			session.getTransaction().commit();
 			
 		}catch(Exception e) {
 			e.printStackTrace();
